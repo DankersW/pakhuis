2c1a770e4cfadea8c043ea617c84507dc9266e8e
DankersW <wouter.dankers@skynet.be>
2022-03-18 16:48:43 +0100

    db: structure

--- db/db.go
+++ db/db.go
@@ -1,1 +1,20 @@
 package db
+
+type db struct {
+	driver string
+}
+
+type Db interface {
+	StoreWsnSensorTelementyData(data []byte) error
+}
+
+func New() (Db, error) {
+	db := db{
+		driver: "hi",
+	}
+	return &db, nil
+}
+
+func (db *db) StoreWsnSensorTelementyData(data []byte) error {
+	return nil
+}
